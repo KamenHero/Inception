481e90715eca3370d42965bf6b19f9902b138d8b
Kamen <oussamaryadi@gmail.com>
2024-03-12 16:58:36 -0400

    .

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,17 +1,15 @@
 #!bin/bash
 
-# echo "im here"
 service mariadb start
-# sleep 2
+
 mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB"
+
 mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PW'"
+
 mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO '$MYSQL_USER'@'%'"
+
 mysql -e "FLUSH PRIVILEGES"
-# mysqladmin -u root -p$MYSQL_ROOTPW shutdown
+
 service mariadb stop
-# mariadb -u root -e "CREATE DATABASE $MYSQL_DB ; CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '123';"
-# mariadb -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DB TO $MYSQL_USER IDENTIFIED BY '123';"
-# mariadb -u root -e "FLUSH PRIVILEGES;"
-# echo "im here 2"
-# exec mysqld_safe
+
 mariadbd
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,15 +1,22 @@
 #! bin/bash
-# sleep infinity
 sleep 10
+
 mkdir /var/www/html/wordpress
+
 cd /var/www/html/wordpress
+
 wp core download --allow-root
-# mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-# rm /var/www/html/wordpress/wp-config.php
+
 wp core config --dbname=${MYSQL_DB} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PW} --dbhost=mariadb --allow-root
+
 wp core install --allow-root --url=${DOMAIN_NAME} --title=Inception --admin_user=${WP_USER} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} 
-wp user create --allow-root $WP_USER_NAME $WP_USER_EMAIL --role=editor --user_pass="$WP_USER_PASSWORD"
+
+wp user create --allow-root $WP_USER_NAME $WP_USER_EMAIL --user_pass="$WP_USER_PASSWORD"
+
 chown -R www-data:www-data /var/www/html/wordpress
+
 sed -i 's#listen = /run/php/php7.4-fpm.sock#listen = 0.0.0.0:9000#g' /etc/php/7.4/fpm/pool.d/www.conf
+
 sed -i 's#chdir = /var/www#chdir = /var/www/html/wordpress#g' /etc/php/7.4/fpm/pool.d/www.conf
+
 php-fpm7.4 -F
